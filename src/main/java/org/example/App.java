f2d63f76a1399f04dfc491d1b18419d9c5c40cd8
rupalpatelj112 <jitindia2003@yahoo.com>
2023-07-16 20:17:20 +0100

    selenium_locator

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.setProperty("webdriver.chrome.driver", "/Users/rupalpatel/Documents/workspace/seleniumSetup/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.id("inputUsername")).sendKeys("Rupal");
+        driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.id("chkboxOne")).click();
+        driver.findElement(By.id("chkboxTwo")).click();
+
+//        driver.findElement(By.className("signInBtn")).click();
+        driver.findElement(By.xpath("//button[contains(@class,'submit')]")).click();
+        //implicit wait
+
+        driver.close();
+
+
+    }
+}
